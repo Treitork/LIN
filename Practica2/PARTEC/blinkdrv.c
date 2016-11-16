62e2b99c3000edc0311ea3327df8d1cdf114c919
puchiki <puchiki@users.noreply.github.com>
2016-11-16 15:26:12 +0100

    Update blinkdrv.c

--- Practica2/PARTEC/blinkdrv.c
+++ Practica2/PARTEC/blinkdrv.c
@@ -23,8 +23,6 @@
 #include <linux/vmalloc.h>
 
 MODULE_LICENSE("GPL");
-/*Marta Rodenas de Miguel & Cristian Pinto Lozano*/
-
 /* Get a minor range for your devices from the usb maintainer */
 #define USB_BLINK_MINOR_BASE	0 
 
