bfa25be29df5d5744d88faa5dc7ee85cf564138c
puchiki <puchiki@users.noreply.github.com>
2016-11-16 14:39:01 +0100

    Update ledsctl_invoke.c

--- Practica2/PARTEB/ledsctl_invoke.c
+++ Practica2/PARTEB/ledsctl_invoke.c
@@ -11,6 +11,9 @@
 #define __NR_LED 316
 #endif
 
+MODULE_DESCRIPTION("LedSctl");
+MODULE_AUTHOR("Marta Rodenas de Miguel & Cristian Pinto Lozano");
+
 long ledctl(int num);
 
 int main(int argc, char **argv) {
