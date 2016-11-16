a79d1f6674688d33bc38435b010a19460e27c918
puchiki <puchiki@users.noreply.github.com>
2016-11-16 04:09:45 +0100

    Update blink_user.c

--- Practica2/OPCIONAL2/blink_user.c
+++ Practica2/OPCIONAL2/blink_user.c
@@ -48,10 +48,24 @@ int get_opt(const char *sec){
 		}
 		return 0;
 	}
-	else if(strcmp(sec,"")==0)
-		printf("option2\n");
-	else if(strcmp(sec,"")==0)
-		printf("option3\n");
+	else if(strcmp(sec,"pares")==0){
+		int val = 0;
+		for(val; val < 5; val++){
+			if(col_pares() != 0)
+				return -1;
+			usleep(1000000);
+		}
+		return 0;
+	}
+	else if(strcmp(sec,"impares")==0){
+		int val = 0;
+		for(val; val < 5; val++){
+			if(col_impares() != 0)
+				return -1;
+			usleep(1000000);
+		}
+		return 0;
+	}
 	else if(strcmp(sec,"off")==0)
 		return power_off_leds();
 	else {
@@ -77,6 +91,68 @@ int random_colors(){
 		str[j][3] = 'x';
 		for( i = 4 ; i < LEDS + 2; i++ ) {
 			str[j][i] = hex_digits[ ( rand() % 16 ) ];
+		}
+		strcat(string, str[j]);
+		if(j != LEDS - 1)
+			strcat(string, ",");
+	}
+	fprintf(fd, "%s", string);
+	free(string);
+	fclose(fd);
+	return 0;
+}
+
+int col_pares(){
+	unsigned char str[LEDS][LEDS] = {};
+	const char *hex_digits = "0123456789ABCDEF";
+	int i, j;
+	time_t t;
+	FILE *fd = fopen(file, "w");
+	if(fd == NULL)
+		return -1;
+	char *string = malloc(SIZE);
+	srand((unsigned) time(&t));
+	for(j = 0; j < LEDS; j+=2){
+		str[j][0] = j + '0';
+		str[j][1] = ':';
+		str[j][2] = '0';
+		str[j][3] = 'x';
+		for( i = 4 ; i < LEDS + 2; i++ ) {
+			str[0][i] = hex_digits[ ( rand() % 16 ) ];
+			str[2][i] = hex_digits[ ( rand() % 16 ) ];
+			str[4][i] = hex_digits[ ( rand() % 16 ) ];
+			str[6][i] = hex_digits[ ( rand() % 16 ) ];
+		}
+		strcat(string, str[j]);
+		if(j != LEDS - 1)
+			strcat(string, ",");
+	}
+	fprintf(fd, "%s", string);
+	free(string);
+	fclose(fd);
+	return 0;
+}
+
+int col_impares(){
+	unsigned char str[LEDS][LEDS] = {};
+	const char *hex_digits = "0123456789ABCDEF";
+	int i, j;
+	time_t t;
+	FILE *fd = fopen(file, "w");
+	if(fd == NULL)
+		return -1;
+	char *string = malloc(SIZE);
+	srand((unsigned) time(&t));
+	for(j = 1; j < LEDS; j+=2){
+		str[j][0] = j + '0';
+		str[j][1] = ':';
+		str[j][2] = '0';
+		str[j][3] = 'x';
+		for( i = 4 ; i < LEDS + 2; i++ ) {
+			str[1][i] = hex_digits[ ( rand() % 16 ) ];
+			str[3][i] = hex_digits[ ( rand() % 16 ) ];
+			str[5][i] = hex_digits[ ( rand() % 16 ) ];
+			str[7][i] = hex_digits[ ( rand() % 16 ) ];
 		}
 		strcat(string, str[j]);
 		if(j != LEDS - 1)
