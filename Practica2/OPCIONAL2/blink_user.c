c47d9deb6be67f6796ccfdac24bae397c0a96be5
Cristian <cripinto@ucm.es>
2016-11-20 14:37:20 +0100

    para entregar

--- Practica2/OPCIONAL2/blink_user.c
+++ Practica2/OPCIONAL2/blink_user.c
@@ -35,8 +35,6 @@ void show_error(){
 	printf("Usage: ./blink_user.c [option]\n");
 	printf("where [option] should be replaced by one of the following options\n");
 	printf("\t- random - generate random colors for the leds\n");
-	printf("\t-  \n");
-	printf("\t-  \n");
 	printf("\t-  off = turn off all the leds\n");
 }
 
@@ -45,24 +43,6 @@ int get_opt(const char *sec){
 		int val = 0;
 		for(val; val < 5; val++){
 			if(random_colors() != 0)
-				return -1;
-			usleep(1000000);
-		}
-		return 0;
-	}
-	else if(strcmp(sec,"pares")==0){
-		int val = 0;
-		for(val; val < 5; val++){
-			if(col_pares() != 0)
-				return -1;
-			usleep(1000000);
-		}
-		return 0;
-	}
-	else if(strcmp(sec,"tres")==0){
-		int val = 0;
-		for(val; val < 5; val++){
-			if(col_tres() != 0)
 				return -1;
 			usleep(1000000);
 		}
@@ -93,67 +73,6 @@ int random_colors(){
 		str[j][3] = 'x';
 		for( i = 4 ; i < LEDS + 2; i++ ) {
 			str[j][i] = hex_digits[ ( rand() % 16 ) ];
-		}
-		strcat(string, str[j]);
-		if(j != LEDS - 1)
-			strcat(string, ",");
-	}
-	fprintf(fd, "%s", string);
-	free(string);
-	fclose(fd);
-	return 0;
-}
-
-int col_pares(){
-	unsigned char str[LEDS][LEDS] = {};
-	const char *hex_digits = "0123456789ABCDEF";
-	int i, j;
-	time_t t;
-	FILE *fd = fopen(file, "w");
-	if(fd == NULL)
-		return -1;
-	char *string = malloc(SIZE);
-	srand((unsigned) time(&t));
-	for(j = 0; j < LEDS; j+=2){
-		str[j][0] = j + '0';
-		str[j][1] = ':';
-		str[j][2] = '0';
-		str[j][3] = 'x';
-		for( i = 4 ; i < LEDS + 2; i++ ) {
-			str[0][i] = hex_digits[ ( rand() % 16 ) ];
-			str[2][i] = hex_digits[ ( rand() % 16 ) ];
-			str[4][i] = hex_digits[ ( rand() % 16 ) ];
-			str[6][i] = hex_digits[ ( rand() % 16 ) ];
-		}
-		strcat(string, str[j]);
-		if(j != LEDS - 1)
-			strcat(string, ",");
-	}
-	fprintf(fd, "%s", string);
-	free(string);
-	fclose(fd);
-	return 0;
-}
-
-int col_tres(){
-	unsigned char str[LEDS][LEDS] = {};
-	const char *hex_digits = "0123456789ABCDEF";
-	int i, j;
-	time_t t;
-	FILE *fd = fopen(file, "w");
-	if(fd == NULL)
-		return -1;
-	char *string = malloc(SIZE);
-	srand((unsigned) time(&t));
-	for(j = 1; j < LEDS; j+=2){
-		str[j][0] = j + '0';
-		str[j][1] = ':';
-		str[j][2] = '0';
-		str[j][3] = 'x';
-		for( i = 4 ; i < LEDS + 2; i++ ) {
-			str[(i+1)%8][i] = hex_digits[ ( rand() % 16 ) ];
-			str[(i+4)%8][i] = hex_digits[ ( rand() % 16 ) ];
-			str[(i+7)%8][i] = hex_digits[ ( rand() % 16 ) ];
 		}
 		strcat(string, str[j]);
 		if(j != LEDS - 1)
